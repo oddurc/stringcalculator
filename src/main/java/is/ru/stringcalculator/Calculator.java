2a771b5fb8283978f40549ef2f39816203161e3c
Oddur Carl Thorarensen <oddur@i-7f3b5dd9.csd07cloud.internal>
2015-10-18 21:39:02 +0000

    testOneNumber

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,9 +2,17 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 
-    public static int add(String text){
-	return 0;
+    public static int add(String text) {
+	if (text.isEmpty()){
+	    return 0;
+	}
+	else {
+	    return toString(text);
+	}
     }
 
+    public static int toString(String string){
+	return Integer.parseInt(string);
+    }
 
 }
